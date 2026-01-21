8ec0a5f8edaab39cd79ff0aeb61e458b0b67ae5b
Demon2050502 <aleksei2050502@mail.ru>
2026-01-21 23:46:33 +0300

    начальная построение файлов,  добавлние бд.

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,1 +1,74 @@
 package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+
+	handler "practice_IstraNet/pkg/handler"
+	mp "practice_IstraNet/pkg/handler/main_paths"
+	postgres "practice_IstraNet/postgres"
+	server "practice_IstraNet/server"
+)
+
+func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error init config: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loading env variable: %s", err.Error())
+	}
+
+	db, err := postgres.NewPostgresDB(postgres.Config{
+		Host: viper.GetString("db.host"),
+		Port: viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName: viper.GetString("db.dbname"),
+		SSLMode: viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		logrus.Fatalf("failed to init DB: %s", err.Error())
+	}
+
+	main_paths := mp.NewMainPaths(db)
+	handlers := handler.NewHandler(main_paths, db)
+	
+	srv := new(server.Server)
+	go func () {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("error running http server: %s", err.Error())
+		}
+	}()
+
+	logrus.Print("Application Started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<- quit
+
+	logrus.Print("Application Shutting Down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error occured on server shutting down: %s", err.Error())
+	}
+
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error occured on DB connoction close: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
---
+++ backend/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS ticket_comments;
+DROP TABLE IF EXISTS ticket_history;
+
+DROP TABLE IF EXISTS tickets;
+
+DROP TABLE IF EXISTS ticket_categories;
+DROP TABLE IF EXISTS ticket_priorities;
+DROP TABLE IF EXISTS ticket_statuses;
+
+DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
